4c8f889216fa263dadcdcae431f4fad593be4fe1
SeregaGS <serega.gaskarov@rambler.ru>
2025-12-12 15:43:47 +0500

    Исправление ошибок для работы js на других страницах

--- js/main.js
+++ js/main.js
@@ -31,135 +31,152 @@ const CLASSNAME = {
   input: '.input',
 }
 
-const headerMenu = DOMElements.header.querySelector(HEADERS.menuList);
-const headerMenuSearch = DOMElements.header.querySelector(HEADERS.menuSearch);
-const headerMenuLogin = DOMElements.header.querySelector(HEADERS.menuLogin);
-const headerMenuCart = DOMElements.header.querySelector(HEADERS.menuCart);
+const initDropdown = () => {
+  if(!DOMElements.header) return;
 
-const closeModalButton = DOMElements.modal.querySelector(CLASSNAME.modalClose);
+  /* DOM ELEMENTS */
+  const headerMenu = DOMElements.header.querySelector(HEADERS.menuList);
+  const headerMenuSearch = DOMElements.header.querySelector(HEADERS.menuSearch);
+  const headerMenuLogin = DOMElements.header.querySelector(HEADERS.menuLogin);
+  const headerMenuCart = DOMElements.header.querySelector(HEADERS.menuCart);
 
-const sliderPrevButton = DOMElements.slider.querySelector(CLASSNAME.sliderButtonPrev);
-const sliderNextButton = DOMElements.slider.querySelector(CLASSNAME.sliderButtonNext);
-const slides = Array.from(DOMElements.slider.querySelectorAll(CLASSNAME.sliderImage));
-const sliderDot = Array.from(DOMElements.slider.querySelectorAll(CLASSNAME.sliderDot));
-const slidesDescription = Array.from(DOMElements.slider.querySelectorAll(CLASSNAME.sliderItem));
-const slidesDots = document.querySelectorAll('.slider__dot');
+  /* DROPDOWN */
+  const closeDropdown = () => {
+    DOMElements.header.querySelectorAll('[data-open="true"]').forEach((el) => {
+      el.dataset.open = "false";
+      el.classList.remove(CLASSNAME.menuLinkActive, CLASSNAME.menuButtonActive);
+    })
+  };
+  const openDropdown = (button, activeClass) => {
+    const isOpen = button.dataset.open === "true";
+    closeDropdown();
+    if(!isOpen) {
+      button.dataset.open = "true";
+      button.classList.add(activeClass);
+    }
+  };
+  const openDropdownButton = (e) => {
+    const clickLink = e.target;
+    e.preventDefault();
+    openDropdown(clickLink, CLASSNAME.menuButtonActive);
+  };
+  const openDropdownButtonLink = (e) => {
+    const clickLink = e.target;
+    if(!clickLink.classList.contains(CLASSNAME.dropdown)) { return }
+    e.preventDefault();
+    openDropdown(clickLink, CLASSNAME.menuLinkActive);
+  };
 
-const SLIDERCOUNT = slides.length;
+  /* DROPDOWN EVENT LISTENER */
+  headerMenu.addEventListener('click', openDropdownButtonLink)
+  headerMenuSearch.addEventListener('click', openDropdownButton);
+  headerMenuLogin.addEventListener('click', openDropdownButton);
+  headerMenuCart.addEventListener('click', openDropdownButton);
 
-let slideIndex = 0;
-
-/* DROPDOWN */
-const closeDropdown = () => {
-  DOMElements.header.querySelectorAll('[data-open="true"]').forEach((el) => {
-    el.dataset.open = "false";
-    el.classList.remove(CLASSNAME.menuLinkActive, CLASSNAME.menuButtonActive);
-  })
 }
-const openDropdown = (button, activeClass) => {
-  const isOpen = button.dataset.open === "true";
-  closeDropdown();
-  if(!isOpen) {
-    button.dataset.open = "true";
-    button.classList.add(activeClass);
-  }
-};
-const openDropdownButton = (e) => {
-  const clickLink = e.target;
-  e.preventDefault();
-  openDropdown(clickLink, CLASSNAME.menuButtonActive);
-}
-const openDropdownButtonLink = (e) => {
-  const clickLink = e.target;
-  if(!clickLink.classList.contains(CLASSNAME.dropdown)) { return }
-  e.preventDefault();
-  openDropdown(clickLink, CLASSNAME.menuLinkActive);
-}
+const initModal = () => {
+  if(!DOMElements.modal || !DOMElements.openModalButton) return;
 
-/* DROPDOWN EVENT LISTENER */
-headerMenu.addEventListener('click', openDropdownButtonLink)
-headerMenuSearch.addEventListener('click', openDropdownButton);
-headerMenuLogin.addEventListener('click', openDropdownButton);
-headerMenuCart.addEventListener('click', openDropdownButton);
-
-/* MODAL POPUP */
-const cleanInput = () => {
-  const modalInput = DOMElements.modal.querySelectorAll(CLASSNAME.input);
-  modalInput.forEach(el => {
-    el.value = '';
-  })
-}
-const openModal = () => {
+  /* DOM ELEMENTS */
+  const closeModalButton = DOMElements.modal.querySelector(CLASSNAME.modalClose);
+  /* MODAL POPUP */
+  const cleanInput = () => {
+    const modalInput = DOMElements.modal.querySelectorAll(CLASSNAME.input);
+    modalInput.forEach(el => {
+      el.value = '';
+    })
+  };
+  const openModal = () => {
     cleanInput();
     DOMElements.modal.classList.add(CLASSNAME.modalShow);
     DOMElements.body.classList.add(CLASSNAME.modalOverlayBody);
-}
-const closeModal =  () => {
+  };
+  const closeModal =  () => {
     DOMElements.modal.classList.remove(CLASSNAME.modalShow);
     DOMElements.body.classList.remove(CLASSNAME.modalOverlayBody);
-}
-const closeModalOverlay = (e) => {
-  if(e.target.closest(CLASSNAME.modalOverlay)) {
-    closeModal();
-  }
-}
-const closeModalKeyPress = (e) => {
-  if(DOMElements.modal.classList.contains(CLASSNAME.modalShow) && e.key === 'Escape') {
-    closeModal();
-  }
-}
-/* MODAL POPUP EVENT LISTENER */
-DOMElements.openModalButton.addEventListener('click', openModal);
-closeModalButton.addEventListener('click', closeModal);
-DOMElements.modal.addEventListener('click', closeModalOverlay);
-document.addEventListener('keydown', closeModalKeyPress)
-
-/* SLIDER */
-const orderSliderElements = (index) => {
-  return (index - slideIndex + SLIDERCOUNT) % SLIDERCOUNT;
-}
-const toggleClassSlider = (slides, className) => {
-  slides.forEach((slide, index) => {
-    slide.classList.toggle(className, index === slideIndex);
-  })
-}
-const updateSliderBackground =  () => {
-  DOMElements.body.classList.forEach(className => {
-    if(className.startsWith('slider-active--')) {
-      DOMElements.body.classList.remove(className);
+  };
+  const closeModalOverlay = (e) => {
+    if(e.target.closest(CLASSNAME.modalOverlay)) {
+      closeModal();
     }
-  })
-  DOMElements.body.classList.add(`slider-active--${slideIndex + 1}`);
+  };
+  const closeModalKeyPress = (e) => {
+    if(DOMElements.modal.classList.contains(CLASSNAME.modalShow) && e.key === 'Escape') {
+      closeModal();
+    }
+  };
+  /* MODAL POPUP EVENT LISTENER */
+  DOMElements.openModalButton.addEventListener('click', openModal);
+  closeModalButton.addEventListener('click', closeModal);
+  DOMElements.modal.addEventListener('click', closeModalOverlay);
+  document.addEventListener('keydown', closeModalKeyPress);
 }
-const toggleClassSliderImage =  (slides, className) => {
-  slides.forEach((slide, index) => {
-    slide.classList.toggle(className, index === slideIndex);
-    slide.style.order = orderSliderElements(index);
-  })
-}
-const updateSlider = () => {
-  toggleClassSliderImage(slides, CLASSNAME.sliderImageActive)
-  updateSliderBackground();
-  toggleClassSlider(sliderDot, CLASSNAME.sliderDotActive);
-  toggleClassSlider(slidesDescription, CLASSNAME.sliderItemActive);
-}
-const updateSliderButtons = (e) => {
-  if(e.target.classList.contains('controls__button--prev')) {
+const initSlider = () => {
+  if(!DOMElements.slider) return;
+
+  /* DOM ELEMENTS */
+  const sliderPrevButton = DOMElements.slider.querySelector(CLASSNAME.sliderButtonPrev);
+  const sliderNextButton = DOMElements.slider.querySelector(CLASSNAME.sliderButtonNext);
+  const slides = Array.from(DOMElements.slider.querySelectorAll(CLASSNAME.sliderImage));
+  const sliderDot = Array.from(DOMElements.slider.querySelectorAll(CLASSNAME.sliderDot));
+  const slidesDescription = Array.from(DOMElements.slider.querySelectorAll(CLASSNAME.sliderItem));
+  const slidesDots = document.querySelectorAll('.slider__dot');
+
+  const SLIDERCOUNT = slides.length;
+
+  let slideIndex = 0;
+
+  /* SLIDER */
+  const orderSliderElements = (index) => {
+    return (index - slideIndex + SLIDERCOUNT) % SLIDERCOUNT;
+  };
+  const toggleClassSlider = (slides, className) => {
+    slides.forEach((slide, index) => {
+      slide.classList.toggle(className, index === slideIndex);
+    })
+  };
+  const updateSliderBackground =  () => {
+    DOMElements.body.classList.forEach(className => {
+      if(className.startsWith('slider-active--')) {
+        DOMElements.body.classList.remove(className);
+      }
+    })
+    DOMElements.body.classList.add(`slider-active--${slideIndex + 1}`);
+  };
+  const toggleClassSliderImage =  (slides, className) => {
+    slides.forEach((slide, index) => {
+      slide.classList.toggle(className, index === slideIndex);
+      slide.style.order = orderSliderElements(index);
+    })
+  };
+  const updateSlider = () => {
+    toggleClassSliderImage(slides, CLASSNAME.sliderImageActive)
+    updateSliderBackground();
+    toggleClassSlider(sliderDot, CLASSNAME.sliderDotActive);
+    toggleClassSlider(slidesDescription, CLASSNAME.sliderItemActive);
+  };
+  const updateSliderButtons = (e) => {
+    if(e.target.classList.contains('controls__button--prev')) {
       slideIndex = (slideIndex - 1 + SLIDERCOUNT) % SLIDERCOUNT;
       updateSlider();
-  } else {
-    slideIndex = (slideIndex + 1) % SLIDERCOUNT;
-    updateSlider();
-  }
-};
+    } else {
+      slideIndex = (slideIndex + 1) % SLIDERCOUNT;
+      updateSlider();
+    }
+  };
 
-/* SLIDER EVENT LISTENER */
-sliderPrevButton.addEventListener('click', updateSliderButtons);
-sliderNextButton.addEventListener('click', updateSliderButtons);
-slidesDots.forEach((dot, index) => {
-  dot.addEventListener('click', () => {
-    slideIndex = index;
-    updateSlider();
-  })
-})
+  /* SLIDER EVENT LISTENER */
+  sliderPrevButton.addEventListener('click', updateSliderButtons);
+  sliderNextButton.addEventListener('click', updateSliderButtons);
+  slidesDots.forEach((dot, index) => {
+    dot.addEventListener('click', () => {
+      slideIndex = index;
+      updateSlider();
+    })
+  });
 
+}
+
+initDropdown();
+initModal();
+initSlider();
